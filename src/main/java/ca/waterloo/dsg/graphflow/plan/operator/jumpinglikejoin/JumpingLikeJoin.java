5d0176e3f6961a1ec46b75c1f2f98dbec7ca1a8c
czy <553101282@qq.com>
2020-12-13 11:06:53 +0800

    测试一下

--- src/main/java/ca/waterloo/dsg/graphflow/plan/operator/jumpinglikejoin/JumpingLikeJoin.java
+++ src/main/java/ca/waterloo/dsg/graphflow/plan/operator/jumpinglikejoin/JumpingLikeJoin.java
@@ -1,4 +1,8 @@
 package ca.waterloo.dsg.graphflow.plan.operator.jumpinglikejoin;
 
+import java.util.List;
+import java.util.Map;
+
 public class JumpingLikeJoin {
+    private Map<Integer, List<Integer>> edgeTable;
 }
